09f6c97e24b062e280861e35657fd51eee0c2633
Alexander Hofer <alexhofer@gmx.ch>
2023-11-12 14:00:32 +0100

    Apply Physics / Controls to Spaceship

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    [SerializeField]
+    private float verticalForceAmount;
+
+    [SerializeField]
+    private float rollTorqueAmount;
+
+    [SerializeField]
+    private float pitchTorqueAmount;
+
+    private float vertInput;
+
+    private float horizInput;
+
+    private float maxPitchAngle = 45f;
+
+    private Rigidbody rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        vertInput = Input.GetAxis("Vertical");
+        horizInput = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        HandlePhysics();
+    }
+
+    private void HandlePhysics()
+    {
+        // Acceleration forward
+        if (vertInput >= 0f)
+        {
+            rigidBody.AddForce(transform.forward * vertInput * verticalForceAmount, ForceMode.Force);
+        }
+
+        // Roll and Pitch with Torque sideways
+        Vector3 rollTorqueSum = transform.up * horizInput * rollTorqueAmount;
+        rigidBody.AddTorque(rollTorqueSum, ForceMode.Force);
+
+        Vector3 pitchTorqueSum = transform.forward * horizInput * pitchTorqueAmount;
+        rigidBody.AddTorque(pitchTorqueSum, ForceMode.Force);
+
+
+        // If we don't have left / right input, we smoothly rotate the pitch rotation back to normal
+        if (horizInput == 0f)
+        {
+            float lerpedAngle = Mathf.LerpAngle(transform.localEulerAngles.z, 0f, Time.fixedDeltaTime * 0.6f);
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, lerpedAngle);
+        }
+    }
+
+
+    /// <summary>
+    /// We use LateUpdate for 2 things:
+    /// 1. We clamp the pitch angle to defined maxPitchAngle to not overshoot the ships rotation
+    /// 2. We freeze the X rotation so the ship doesn't go down with the nose if we rotate it
+    /// </summary>
+    private void LateUpdate()
+    {
+        float clampedAngleZ = ClampAngle(transform.localEulerAngles.z, -maxPitchAngle, maxPitchAngle);
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, clampedAngleZ);
+    }
+
+    /// <summary>
+    ///  Clamps provided angle to min / max angle
+    ///  
+    ///  Copied from: https://gist.github.com/johnsoncodehk/2ecb0136304d4badbb92bd0c1dbd8bae
+    /// </summary>
+    private float ClampAngle(float angle, float min, float max)
+    {
+        float start = (min + max) * 0.5f - 180;
+        float floor = Mathf.FloorToInt((angle - start) / 360) * 360;
+        return Mathf.Clamp(angle, min + floor, max + floor);
+    }
+}
