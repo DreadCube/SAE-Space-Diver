c4202c866018a4b916aa2f5d0723e622e3d7a611
Alexander Hofer <alexhofer@gmx.ch>
2023-11-13 16:11:22 +0100

    Update Logic of CameraController. Add velocity of target into acccount. Re enable UI Elements

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,12 +7,34 @@ public class CameraController : MonoBehaviour
     [SerializeField]
     private Transform target;
 
-    private void Update()
+    private Rigidbody targetRigidbody;
+
+    private float forwardOffset = 50f;
+
+    private void Awake()
     {
-        // Set the rotation of camera to default
+        targetRigidbody = target.GetComponent<Rigidbody>();
+    }
+
+    private void LateUpdate()
+    {
+        /**
+         * Our Camera position is based on the position of the target.
+         * We use a default forward Offset and the sqrtMagnitute of the target as an
+         * additional "velocity" offset.
+         */
+        float sqrtMagnitude = targetRigidbody.velocity.sqrMagnitude;
+        float velocityOffset = Mathf.Clamp(sqrtMagnitude / 500, 1f, 20f);
+        transform.position = target.position - (target.forward * (forwardOffset + velocityOffset));
+
+        // To prevent up and down jigglering we force Y position to zero.
+        transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
+
+        // We always keep the default rotation of the camera
         transform.rotation = Quaternion.identity;
 
-        // Look at our target
+
+        // Look to the target
         transform.LookAt(target.transform);
     }
 }
