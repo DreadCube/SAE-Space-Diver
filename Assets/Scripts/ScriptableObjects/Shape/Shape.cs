5f61b866db0a95aa906f2e240e6b4dc98c5e6902
Alexander Hofer <alexhofer@gmx.ch>
2023-11-15 21:36:07 +0100

    Create a basic InventoryManager. We use ScriptableObject that are used as our items (later on for more). The InventoryManager is capable of Sorting the items based on amoun / hue and shapeType with asc / desc. Every SortType uses a different sorting mechanism. Implemented are: InsertionSort / MergeSort and QuickSort

---
+++ Assets/Scripts/ScriptableObjects/Shape/Shape.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Shape", menuName = "ScriptableObjects/Shape", order = 1)]
+public class Shape : ScriptableObject
+{
+    public enum ShapeType
+    {
+        Cube,
+        Circle
+    }
+
+    [field: SerializeField] public ShapeType Type { get; private set; }
+
+    [field: SerializeField] public Color Color { get; private set; }
+
+    public float Hue
+    {
+        get
+        {
+            Color.RGBToHSV(Color, out float h, out _, out _);
+
+            return h;
+        }
+    }
+}
