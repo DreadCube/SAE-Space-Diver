13634c1ea94eab3e4b80234cc62c299556a377f1
Alexander Hofer <alexhofer@gmx.ch>
2023-11-21 11:43:31 +0100

    Add some comments to the UIManager

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -25,6 +25,76 @@ public class UIManager : MonoBehaviour
     private string classNamePrimarySelected = "button-primary-selected";
     private string classNameInventoryItem = "inventory-item";
 
+    /**
+     * Based on the current sorted Inventory Items
+     * this draws the inventory UI
+     */
+    public void DrawInventoryUI()
+    {
+        List<InventoryItem> inventoryItems = InventoryManager.Instance.GetInventoryItems();
+
+        // Clear our Items in the UI
+        inventoryItemsRoot.Clear();
+
+
+        // Reset selected styles of amount / hue / shape Button (if exists)
+        amountButton.RemoveFromClassList(classNamePrimarySelected);
+        hueButton.RemoveFromClassList(classNamePrimarySelected);
+        shapeButton.RemoveFromClassList(classNamePrimarySelected);
+
+        // Add the selected styles to the correct button
+        switch (InventoryManager.Instance.GetActiveSortType())
+        {
+            case SortType.Amount:
+                amountButton.AddToClassList(classNamePrimarySelected);
+                break;
+            case SortType.Hue:
+                hueButton.AddToClassList(classNamePrimarySelected);
+
+                break;
+            default:
+                shapeButton.AddToClassList(classNamePrimarySelected);
+                break;
+        }
+
+        // Sets selected Styles for the correct Asc / Desc Button
+        if (InventoryManager.Instance.GetActiveSortDirection() == InventoryManager.SortDirection.Asc)
+        {
+            ascButton.AddToClassList(classNamePrimarySelected);
+            descButton.RemoveFromClassList(classNamePrimarySelected);
+        }
+        else
+        {
+            descButton.AddToClassList(classNamePrimarySelected);
+            ascButton.RemoveFromClassList(classNamePrimarySelected);
+        }
+
+        /**
+         * For every inventoryItem we create the proper Button and apply the correct
+         * backgroundImage / tintColor and item Count.
+         * 
+         * At the end we add the itemButton to our Inventory Items Root as child.
+         */
+        for (int i = 0; i < inventoryItems.Count; i++)
+        {
+            InventoryItem item = inventoryItems[i];
+
+            Button itemButton = new Button();
+            itemButton.AddToClassList(classNameInventoryItem);
+
+            itemButton.style.backgroundImage = item.GetShape().UITexture;
+
+
+            // TODO: Add Controls for switching between current active item
+            Color shapeColor = item.GetShape().Color;
+            itemButton.style.unityBackgroundImageTintColor = shapeColor;
+
+            itemButton.text = item.GetAmount().ToString();
+
+            inventoryItemsRoot.Add(itemButton);
+        }
+    }
+
 
     private void Awake()
     {
@@ -44,6 +114,14 @@ public class UIManager : MonoBehaviour
         DrawInventoryUI();
     }
 
+    /**
+     * EnableInventoryUI registers the needed functionality of our Inventory UI.
+     * This includes:
+     * 1. Reference our Buttons from the UI so we can work with them
+     * 2. Register proper on click callbacks for the buttons
+     * 3. Register proper KeyPress Events so we can control the Inventory UI with
+     *    keypresses
+     */
     private void EnableInventoryUI()
     {
         inventoryRoot = UIDocument.rootVisualElement.Q<VisualElement>("Inventory");
@@ -110,61 +188,6 @@ public class UIManager : MonoBehaviour
         SortType activeSortType = InventoryManager.Instance.GetActiveSortType();
         InventoryManager.Instance.SortInventoryItems(activeSortType, sortDirection);
         DrawInventoryUI();
-    }
-
-    public void DrawInventoryUI()
-    {
-        List<InventoryItem> inventoryItems = InventoryManager.Instance.GetInventoryItems();
-
-        inventoryItemsRoot.Clear();
-
-        amountButton.RemoveFromClassList(classNamePrimarySelected);
-        hueButton.RemoveFromClassList(classNamePrimarySelected);
-        shapeButton.RemoveFromClassList(classNamePrimarySelected);
-
-        switch (InventoryManager.Instance.GetActiveSortType())
-        {
-            case SortType.Amount:
-                amountButton.AddToClassList(classNamePrimarySelected);
-                break;
-            case SortType.Hue:
-                hueButton.AddToClassList(classNamePrimarySelected);
-
-                break;
-            default:
-                shapeButton.AddToClassList(classNamePrimarySelected);
-                break;
-        }
-
-        if (InventoryManager.Instance.GetActiveSortDirection() == InventoryManager.SortDirection.Asc)
-        {
-            ascButton.AddToClassList(classNamePrimarySelected);
-            descButton.RemoveFromClassList(classNamePrimarySelected);
-        }
-        else
-        {
-            descButton.AddToClassList(classNamePrimarySelected);
-            ascButton.RemoveFromClassList(classNamePrimarySelected);
-        }
-
-        for (int i = 0; i < inventoryItems.Count; i++)
-        {
-            InventoryItem item = inventoryItems[i];
-
-            Button itemButton = new Button();
-            itemButton.AddToClassList(classNameInventoryItem);
-
-            itemButton.style.backgroundImage = item.GetShape().UITexture;
-
-
-            // TODO: Add Controls for switching between current active item
-            Color shapeColor = item.GetShape().Color;
-            itemButton.style.unityBackgroundImageTintColor = shapeColor;
-
-            itemButton.text = item.GetAmount().ToString();
-
-            inventoryItemsRoot.Add(itemButton);
-        }
     }
 }
 
