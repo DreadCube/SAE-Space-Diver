bb82c55c39fdccb6d754a7866f1c422ee6281bad
Alexander Hofer <alexhofer@gmx.ch>
2023-11-28 18:51:40 +0100

    Simplify code in Bullet.cs

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,25 +7,15 @@ public class Bullet : ShapeMonoBehaviour
 
     private float maxLiveTime = 1f;
 
-    private float startTime;
-
     private void Start()
     {
         bulletRigidbody = GetComponent<Rigidbody>();
-        startTime = Time.timeSinceLevelLoad;
+
+        Destroy(gameObject, maxLiveTime);
     }
 
     private void FixedUpdate()
     {
         bulletRigidbody.AddRelativeForce(Vector3.forward * 1000f, ForceMode.VelocityChange);
-
-        /**
-         * We destroy the Bullet if the max live time is reached.
-         * TODO: Supi fragen. FixedUpdate oder normaler Update?
-         */
-        if (Time.timeSinceLevelLoad - startTime > maxLiveTime)
-        {
-            Destroy(gameObject);
-        }
     }
 }
